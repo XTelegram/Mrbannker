f5e19ba71d927e5a028bf2f45913f2d9e3779cdd
Rahul <87630115+XTelegram@users.noreply.github.com>
2022-10-04 17:35:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,7 +107,7 @@ async def binio(message: types.Message):
     head={'content-type':'application/x-www-form-urlencoded; charset=UTF-8'}
     data = f"link= {BIN}&pass=&boxlinklist=0"
     cookie= {'amember_ru':'rahulnova','amember_rp':'41b8739da7a1766af530a993a4a5c74d50bd1047','cookieconsent_status':'dismiss','crisp-client%2Fsession%2Fffca7959-dfd6-4915-90e2-b2feb57ae28e':'session_f5e79faa-8343-424c-a3cf-7241c05fd264','cf_clearance':'zsY5UafHGge9gvyNDPrjyaGI8oYnuOgf_.uUZ6CrXok-1655790566-0-150;adv1_closed=1','afscr8':'fIGyoi6Zo7pYdDF%2BivnhmPa7wwsNdWl5','PHPSESSID':'20bfbaf6632e67d47af4691b6dccdaf3'}
-    resp = requests.post(url, data=data,  headers=head,cookies=cookie).json
+    resp = requests.post(url, data=data,  headers=head,cookies=cookie)
     y=resp.json()
     olink=y["original_link"]
     fname=y["filename"]
